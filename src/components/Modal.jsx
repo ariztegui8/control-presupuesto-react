e510b0e9a79eaafd2f990848cccb18b87c531a26
Jorge Ariztegui <jorgeariztegui8@gmail.com>
2022-04-07 22:37:14 -0300

    se agrego inputsFormulario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useState } from "react";
 
 const Modal = ({setModal}) => {
+
+    const [inputsFormulario, setInputsFormulario] = useState({
+        nombre: '',
+        cantidad: '',
+        categoria: ''
+    })
+
+    const {nombre, cantidad, categoria} = inputsFormulario;
+
+    const handleChange = e =>{
+        setInputsFormulario({
+            ...inputsFormulario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(inputsFormulario);
+    }
 
     const ocultarModal = ()=>{
         setModal(false)
@@ -15,6 +35,62 @@ const Modal = ({setModal}) => {
                 className="cerrarModal"
             >Cerrar Modal</button>
         </div>
+
+        <form
+            className='formulario'
+            onSubmit={handleSubmit}
+        >
+            <legend>Nuevo Gasto</legend>
+
+            <div className='input-form'>
+                <label htmlFor="nombre">Nombre Gasto</label>
+                <input
+                    type="text"
+                    placeholder='Añade el Nombre del gasto'
+                    id='nombre'
+                    name="nombre"
+                    value={nombre}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className='input-form'>
+                <label htmlFor="cantidad">Cantidad</label>
+                <input
+                    type="number"
+                    placeholder='Añade la cantidad del gasto'
+                    id='cantidad'
+                    name="cantidad"
+                    value={cantidad}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className='input-form'>
+                <label htmlFor="categoria">Categoria</label>
+                <select
+                    id='categoria'
+                    name="categoria"
+                    value={categoria}
+                    onChange={handleChange}
+                >
+                    <option value="">-- Seleccione --</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos</option>
+                    <option value="salud">Salud</option>
+                    <option value="vicios">Vicios</option>
+                </select>
+            </div>
+
+            <input
+                type="submit"
+                value="Añadir Gasto"
+                className='input-submit'
+             />
+
+        </form>
     </div>
   )
 }
